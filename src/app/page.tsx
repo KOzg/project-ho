47f8316e673ec09f0d57b4ff662de50765b8fbf5
Ozgunay, Kaan <Kaan.Ozgunay@wonderful.com>
2025-08-09 11:19:21 -0700

    switch to sass and clean-up main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
         <div>
-          <h1>{`Welcome to Kaan Ozgunay's portfolio site`}</h1>
+          <h1>{`Under Construction`}</h1>
           <nav>
             <ul>
-              <li>
-                <a href="/ho">Project HO!</a>
-              </li>
-              <li>
-                <a href="/cv">CV</a>
-              </li>
-              <li>
-                <a href="/about">About the site</a>
-              </li>
-              <li>
-                <a href="/vibe">Vibe Projects</a>
-              </li>
             </ul>
           </nav>
         </div>
