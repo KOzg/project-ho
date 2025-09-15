f06a01e8eb1780888a86d683c38190a9cb9de44d
Kaan Ozgunay <kaanozgunay@Kaans-MacBook-Air.local>
2025-09-14 18:01:39 -0700

    clean-up imports

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,9 +1,7 @@
 import TodoList from '@/components/SimpleTodo/TodoList/TodoList';
-import Todo from '@/components/ToDo/Todo/Todo'
 import Clock from '@/components/Clock/Clock';
 import Kanban from '@/components/Kanban/Kanban';
 import { SimpleTodoProvider } from '@/components/SimpleTodo/SimpleTodoProvider';
-import { TodoProvider } from '@/components/ToDo/TodoProvider';
 
 // import { getAlbums } from './api';
 
@@ -16,9 +14,6 @@ export default async function Lab() {
       <SimpleTodoProvider>
         <TodoList />
       </SimpleTodoProvider>
-      <TodoProvider>
-        <Todo />
-      </TodoProvider>
 
       {/*<AlbumCarousel/>*/}
     </>
